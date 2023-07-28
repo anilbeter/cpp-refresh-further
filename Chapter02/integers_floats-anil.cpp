4c541046b4dd773bcf8a0f1923e72ef7e41ca8b9
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-28 17:45:01 +0300

    add integers floats chars

---
+++ Chapter02/integers_floats-anil.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdint>
+
+int main()
+{
+
+    int i = 0;
+
+    std::int8_t i1 = 0;
+    std::uint8_t i1 = 0U;
+
+    std::int16_t i1 = 0;
+    std::uint16_t i1 = 0U;
+
+    std::int32_t i1 = 0;
+    std::uint32_t i1 = 0U;
+
+    std::int64_t i1 = 0;
+    std::uint64_t i1 = 0ULL;
+
+    float f1 = 12.0f; // 32 bit
+    double f2 = 12.0; // 64 bit
+
+    bool b1 = true;
+    bool b2 = false;
+
+    char c1 = 'A';
+    char c2 = 'b';
+
+    return 0;
+}
